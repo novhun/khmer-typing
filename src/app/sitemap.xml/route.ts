9bf54dc3c0ea54be2b143ccd470607e1db76a828
HUN <suybrake55@gmail.com>
2026-09-10 20:23:48 +0700

    fix(seo): use absolute URL for sitemap.xsl in xml-stylesheet header

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -113,7 +113,7 @@ ${alternatesXml}    <lastmod>${now}</lastmod>
     .join("\n");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
+<?xml-stylesheet type="text/xsl" href="${baseUrl}/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 ${xmlEntries}
 </urlset>`;
